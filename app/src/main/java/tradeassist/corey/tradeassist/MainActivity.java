506f3e298288f090415dede5be7486f585f5c9d8
craig <coreymcmurray14@gmail.com>
2022-09-26 17:02:33 -0600

    Started working on button to update depending on whether the shareSize, profitTarget or stopLoss were changed.

--- app/src/main/java/tradeassist/corey/tradeassist/MainActivity.java
+++ app/src/main/java/tradeassist/corey/tradeassist/MainActivity.java
@@ -13,13 +13,17 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     private static final DecimalFormat formatter = (DecimalFormat)DecimalFormat.getInstance(Locale.getDefault());
 
-    private TextView    txtMaxShareSizeValue;
-    private TextView    txtProfitTarget;
-    private TextView    txtStopLoss;
+    private int         shareSize;
+    private float       profitTarget;
+    private float       stopLoss;
 
     private EditText    edtTotalCapital;
     private EditText    edtStockPrice;
     private EditText    edtMaxLoss;
+    private EditText    edtMaxShareSizeValue;
+    private EditText    edtProfitTarget;
+    private EditText    edtStopLoss;
+
 
 
     @Override
@@ -35,42 +39,65 @@ public class MainActivity extends AppCompatActivity {
      * This function provides the initialization of the graphical elements of this activity.
      */
     private void initGui() {
-        // TextViews
-        txtMaxShareSizeValue    = findViewById(R.id.txtSharesValue);
-        txtProfitTarget         = findViewById(R.id.txtProfitTargetValue);
-        txtStopLoss             = findViewById(R.id.txtStopLossValue);
 
         // EditText
         edtTotalCapital         = findViewById(R.id.edtTotalCapital);
         edtStockPrice           = findViewById(R.id.edtStockPrice);
         edtMaxLoss              = findViewById(R.id.edtMaxLoss);
+        edtMaxShareSizeValue    = findViewById(R.id.edtMaxShareSize);
+        edtProfitTarget         = findViewById(R.id.edtProfitTarget);
+        edtStopLoss             = findViewById(R.id.edtStopLoss);
     }
 
     public void onCrunch(View view) {
+
         float totalCapital  = safeParseFloat(edtTotalCapital.getText().toString(), 0.0f);
         float stockPrice    = safeParseFloat(edtStockPrice.getText().toString(), 0.0f);
         float maxLoss       = safeParseFloat(edtMaxLoss.getText().toString(), 1.0f)/100.0f;
-//        float profitTarget  = safeParseFloat(txtProfitTarget.getText().toString(), 0.0f);
-//        float stopLoss      = safeParseFloat(txtStopLoss.getText().toString(), 0.0f);
+//        float profitTarget  = safeParseFloat(edtProfitTarget.getText().toString(), 0.0f);
+//        float stopLoss      = safeParseFloat(edtStopLoss.getText().toString(), 0.0f);
 
         int maxShareSize    = (int) (totalCapital/stockPrice);
 
         float profitTarget = ((maxLoss*2.0f)*totalCapital)/maxShareSize;
         float stopLoss = (maxLoss*totalCapital)/maxShareSize;
 
-        txtMaxShareSizeValue.setText(String.valueOf(maxShareSize));
-        txtProfitTarget.setText(String.format(Locale.getDefault(),"%.2f", profitTarget));
-        txtStopLoss.setText(String.format(Locale.getDefault(), "%.2f", stopLoss));
+        this.shareSize      = maxShareSize;
+        this.profitTarget   = profitTarget;
+        this.stopLoss       = stopLoss;
 
-
-        if(stopLoss < 0.1f) {
-
-        }
-
+        edtMaxShareSizeValue.setText(String.valueOf(maxShareSize));
+        edtProfitTarget.setText(String.format(Locale.getDefault(),"%.2f", profitTarget));
+        edtStopLoss.setText(String.format(Locale.getDefault(), "%.2f", stopLoss));
 
     }
 
+    public void onUpdate(View view) {
+        float totalCapital  = safeParseFloat(edtTotalCapital.getText().toString(), 0.0f);
+        float stockPrice    = safeParseFloat(edtStockPrice.getText().toString(), 0.0f);
+        float maxLoss       = safeParseFloat(edtMaxLoss.getText().toString(), 1.0f)/100.0f;
+        float profitTarget  = safeParseFloat(edtProfitTarget.getText().toString(), 0.0f);
+        float stopLoss      = safeParseFloat(edtStopLoss.getText().toString(), 0.0f);
+        int maxShareSize    = safeParseInt(edtMaxShareSizeValue.getText().toString(), 0);
 
+
+
+
+
+        if(this.shareSize != maxShareSize) {
+            shareSize = maxShareSize;
+
+            profitTarget = ((maxLoss*2.0f)*totalCapital)/maxShareSize;
+            stopLoss = (maxLoss*totalCapital)/maxShareSize;
+        }
+
+
+        edtMaxShareSizeValue.setText(String.valueOf(maxShareSize));
+        edtProfitTarget.setText(String.format(Locale.getDefault(),"%.2f", profitTarget));
+        edtStopLoss.setText(String.format(Locale.getDefault(), "%.2f", stopLoss));
+
+
+    }
 
     private float safeParseFloat(String source, float defaultValue) {
         float   result = defaultValue;
